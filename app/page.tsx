f4b3d73769fc8afe62abb274585d685d36f9674d
roy422 <roy422roy@gmail.com>
2025-09-22 11:31:35 +0800

    在首頁中新增 YcCard 組件，替換一個 RoyCard 組件以豐富內容展示。

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import RoyCard from "@/card/RoyCard";
 import PatrickStarCard from "@/card/PatrickStarCard";
 import WesleyCard from "@/card/WesleyCard";
+import YcCard from "@/card/ycCard";
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen gap-4 bg-gradient-to-br from-red-400 via-green-500 to-yellow-500">
       <RoyCard />
       <PatrickStarCard />
-      <RoyCard />
+      <YcCard />
       <WesleyCard />
     </div>
   );
