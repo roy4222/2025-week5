1f251866b2521187781f05aa679e0b0eba676340
roy422 <roy422roy@gmail.com>
2025-10-13 10:34:49 +0800

    Refactor homepage to replace card components with navigation links for improved user access to product, customer, animate, and store pages.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
-import RoyCard from "@/card/RoyCard";
-import PatrickStarCard from "@/card/PatrickStarCard";
-import WesleyCard from "@/card/WesleyCard";
-import YcCard from "@/card/ycCard";
+import Link from "next/link";
 
 export default function Home() {
+  const links = [
+    { href: "/product", text: "Product" },
+    { href: "/customer", text: "Customer" },
+    { href: "/animate", text: "Animate" },
+    { href: "/store", text: "Store" },
+  ];
+
   return (
-    <div className="flex justify-center items-center h-screen gap-4 bg-gradient-to-br from-red-400 via-green-500 to-yellow-500">
-      <RoyCard />
-      <PatrickStarCard />
-      <YcCard />
-      <WesleyCard />
+    <div className="flex justify-center items-center h-screen gap-4 bg-white">
+      {links.map((link) => (
+        <Link key={link.href} className="text-blue-500" href={link.href}>
+          {link.text}
+        </Link>
+      ))}
     </div>
   );
 }
---
+++ app/store/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+export default function Store() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-white">
+      <h1>Store</h1>
+      <Link href="/" className="bg-blue-500 text-white p-2 rounded-md">
+        go to home
+      </Link>
+    </div>
+  );
+}
